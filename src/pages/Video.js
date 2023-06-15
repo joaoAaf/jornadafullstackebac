a396a54b3bc88c5bd4e83c2188a18a1b0c2efa82
joaoAaf <joao.anderson.af@gmail.com>
2023-06-14 21:52:41 -0300

    Criação e implementação do componente   Video.js

---
+++ src/pages/Video.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Video.css";
+
+function Video() {
+  return (
+    <div className="video">
+      Eu sou um componente Vídeo
+      <video className="video__player"></video>
+    </div>
+  );
+}
+
+export default Video;
