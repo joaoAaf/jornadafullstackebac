f3cfc7f2aafa1c153ce269ca00c9c65288840090
joaoAaf <joao.anderson.af@gmail.com>
2023-06-14 21:07:15 -0300

    Estilização app__videos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+     <div className='app__videos'>
+      Vídeos
+      Vídeos
+      Vídeos
+     </div>
     </div>
   );
 }
