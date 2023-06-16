a215a490b69c37984edf125495e0ca38716c59f8
joaoAaf <joao.anderson.af@gmail.com>
2023-06-16 15:49:17 -0300

    Criação do componente Footer

---
+++ src/pages/components/footer/VideoFooter.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./VideoFooter.css";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+
+function VideoFooter() {
+  return (
+    <div className="videoFooter">
+      <div className="videoFooter__text">
+        <h3>@João Anderson</h3>
+        <p>Descrição Vídeo</p>
+        <div className="videoFooter__music">
+          <MusicNoteIcon />
+          <div className="videoFooter__textMusic">
+            <p>Título Música</p>
+          </div>
+        </div>
+      </div>
+      <img
+        className="videoFooter__record"
+        alt="Imagem de um vinil girando"
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/vinil.png?t=2023-05-22T19%3A39%3A28.772Z"
+      />
+    </div>
+  );
+}
+
+export default VideoFooter;
